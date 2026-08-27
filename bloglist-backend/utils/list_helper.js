5a788fb7d4e5246ccd07a0e1f0734cfe64f433a0
carloscarrillocruz18-del <carloscarrillocruz18@gmail.com>
2026-08-27 16:46:04 -0400

    Completado Tareas 4.3, 4.4 y 4.5: funciones auxiliares y pruebas unitarias

---
+++ bloglist-backend/utils/list_helper.js
@@ -0,0 +1,21 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((prev, current) => {
+    return (current.likes > prev.likes) ? current : prev
+  })
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+}
